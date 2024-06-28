2c5a4d1626aa2b921486be7a51f0d296f4abf3f1
Karina Boronina <Gubashina@yandex.ru>
2024-06-28 15:39:28 +0300

    переписан слайдер и добавлены карточки второй секции

--- js/script.js
+++ js/script.js
@@ -1,105 +1,15 @@
-const TOTAL_CARD_COUNT = document.querySelectorAll('.player').length;
-const playersBtns = document.querySelector('.players__slider_btns');
-const leftBtn = document.querySelector('.left');
-const rightBtn = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const sliderList = document.querySelector('.players__list');
-const countSlide = document.querySelector('.count-slide');
-const gapSize = 20;
-let sliderWidth = parseInt(getComputedStyle(slider).width);
-let screenWidth = window.innerWidth;
-let cardCountOnSlide = getCardCountOnSlide();
-let hiddenCardCount = calcHiddenCardCount();
-let totalGap = gapSize * (cardCountOnSlide + 1);
-let cardSize = calcCardSize();
-let currentCard = cardCountOnSlide;
-let translateShift = 0;
-countSlide.textContent = currentCard;
-document.documentElement.style.setProperty('--cardSize', cardSize + 'px');
+const playersSection = document.querySelector('.players');
+const stagesContent = document.querySelector('.stages__content');
 
-//Для управления изначальной стилизации кнопки по условию видимости
-if (currentCard - 1 < cardCountOnSlide) {
-    leftBtn.disabled = true;
-}
+const playersCards = playersInfo.map((el) => createPlayerCard(el));
+const playersSlider = new Slider({ title: 'Участники турнира', slides: playersCards, root: playersSection });
+const stagesCards = allocationItem(stagesCardInfo);
+const stagesSlider = new Slider({ this: '', slides: stagesCards, root: stagesContent });
 
-window.addEventListener('resize', onresize);
-
-function onresize() {
-    sliderWidth = parseInt(getComputedStyle(slider).width);
-    screenWidth = window.innerWidth;
-    cardCountOnSlide = getCardCountOnSlide();
-    hiddenCardCount = calcHiddenCardCount();
-    currentCard = cardCountOnSlide;
-    countSlide.textContent = currentCard;
-    totalGap = getTotalGap();
-    cardSize = calcCardSize();
-    translateShift = 0;
-    document.documentElement.style.setProperty('--cardSize', cardSize + 'px');
-    sliderList.style.translate = -(cardSize + gapSize) * translateShift + 'px';
-}
-
-function getCardCountOnSlide() {
-    if (screenWidth > 820) {
-        return 3;
-    } else if (520 < screenWidth && screenWidth < 820) {
-        return 2;
-    } else {
-        return 1;
-    }
-}
-
-function getTotalGap() {
-    return gapSize * (cardCountOnSlide + 1);
-}
-
-function calcCardSize() {
-    return (sliderWidth - totalGap) / cardCountOnSlide;
-}
-
-function calcHiddenCardCount() {
-    return TOTAL_CARD_COUNT - cardCountOnSlide;
-}
-
-function moveLeft() {
-    if (currentCard + 1 > TOTAL_CARD_COUNT) rightBtn.disabled = false;
-
-    if (currentCard - 1 < cardCountOnSlide) {
-        leftBtn.disabled = true;
-        return true;
-    }
-    // if (currentCard - 1 === cardCountOnSlide) {
-    //     leftBtn.disabled = true;
-    // }
-    currentCard--;
-    countSlide.textContent = currentCard;
-    sliderList.style.translate = -((cardSize + gapSize) * currentCard - (cardSize + gapSize) * cardCountOnSlide) + 'px';
-}
-
-function moveRight() {
-    if (currentCard + 1 >= cardCountOnSlide) leftBtn.disabled = false;
-    if (currentCard + 1 > TOTAL_CARD_COUNT) {
-        rightBtn.disabled = false;
-        return true;
-    }
-    if (currentCard + 1 === TOTAL_CARD_COUNT) {
-        rightBtn.disabled = true;
-    }
-    currentCard++;
-    countSlide.textContent = currentCard;
-    sliderList.style.translate = -((cardSize + gapSize) * currentCard - (cardSize + gapSize) * cardCountOnSlide) + 'px';
-}
-
-playersBtns.addEventListener('click', (e) => {
-    if (e.target.matches('.left')) {
-        moveLeft();
-    } else {
-        moveRight();
-    }
+window.addEventListener('resize', () => {
+    playersSlider.updateSize();
+    stagesSlider.updateSize();
 });
 
-setInterval(() => {
-    if (moveRight()) {
-        currentCard = cardCountOnSlide;
-        countSlide.textContent = currentCard;
-    }
-}, 4000);
+playersSlider.mount();
+stagesSlider.mount();
---
+++ js/sliderCards.js
@@ -0,0 +1,75 @@
+const playersInfo = [
+    {
+        image: './img/profile.png',
+        name: 'Хозе-Рауль Капабланка',
+        dignity: 'Чемпион мира по шахматам',
+    },
+    { image: './img/profile.png', name: 'Эммануил Ласкер', dignity: 'Чемпион мира по шахматам' },
+    { image: './img/profile.png', name: 'Александр Алехин', dignity: 'Чемпион мира по шахматам' },
+    { image: './img/profile.png', name: 'Арон Нимцович', dignity: 'Чемпион мира по шахматам' },
+    { image: './img/profile.png', name: 'Рихард Рети', dignity: 'Чемпион мира по шахматам' },
+    { image: './img/profile.png', name: 'Остап Бендер', dignity: 'Чемпион мира по шахматам' },
+];
+
+const stagesCardInfo = [
+    'Строительство железнодорожной магистрали Москва-Васюки',
+    'Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов',
+    'Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет',
+    'Строительство дворца для турнира',
+    'Размещение гаражей для гостевого автотранспорта',
+    'Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов',
+    'Создание аэропорта «Большие Васюки» с регулярным отправлением почтовых самолётов и дирижаблей во все концы света, включая Лос-Анжелос и Мельбурн',
+];
+
+function createPlayerCard(player) {
+    const div = document.createElement('div');
+    div.className = 'player';
+
+    div.innerHTML = `
+    <img class='player__img' src=${player.image} alt='profile' />
+    <h4 class='player__name'>${player.name}</h4>
+    <p class='player__info'>${player.dignity}</p>
+    <button class='btn__oval'>Подробнее</button>
+    `;
+    return div;
+}
+
+function createStagesItem(str, i) {
+    const p = document.createElement('p');
+    p.className = 'stages__item';
+
+    p.innerHTML = `
+    <span class="stages__num">${i + 1}</span> 
+    <span class="stages__text">${str}</span>
+    `;
+
+    return p;
+}
+
+function createStagesCard() {
+    const div = document.createElement('div');
+    div.className = 'stages__card';
+
+    return div;
+}
+
+function allocationItem(stagesInfoArr) {
+    const arr = [];
+    let card;
+
+    for (let i = 0; i < stagesInfoArr.length; ) {
+        if (stagesInfoArr[i].length > 80) {
+            card = createStagesCard();
+            card.append(createStagesItem(stagesInfoArr[i], i));
+            arr.push(card);
+            i++;
+        } else {
+            card = createStagesCard();
+            card.append(createStagesItem(stagesInfoArr[i], i), createStagesItem(stagesInfoArr[i + 1], i + 1));
+            arr.push(card);
+            i += 2;
+        }
+    }
+
+    return arr;
+}
