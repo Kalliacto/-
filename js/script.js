c00ef6adb0695a62c9ee4f708bc3d749b5a36c1e
Karina Boronina <Gubashina@yandex.ru>
2024-06-28 08:24:54 +0300

    адаптивный слайдер участников

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const TOTAL_CARD_COUNT = document.querySelectorAll('.player').length;
 const playersBtns = document.querySelector('.players__slider_btns');
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const sliderList = document.querySelector('.players__list');
+const countSlide = document.querySelector('.count-slide');
 const gapSize = 20;
 let sliderWidth = parseInt(getComputedStyle(slider).width);
 let screenWidth = window.innerWidth;
@@ -13,7 +14,13 @@ let totalGap = gapSize * (cardCountOnSlide + 1);
 let cardSize = calcCardSize();
 let currentCard = cardCountOnSlide;
 let translateShift = 0;
+countSlide.textContent = currentCard;
 document.documentElement.style.setProperty('--cardSize', cardSize + 'px');
+
+//Для управления изначальной стилизации кнопки по условию видимости
+if (currentCard - 1 < cardCountOnSlide) {
+    leftBtn.disabled = true;
+}
 
 window.addEventListener('resize', onresize);
 
@@ -23,12 +30,12 @@ function onresize() {
     cardCountOnSlide = getCardCountOnSlide();
     hiddenCardCount = calcHiddenCardCount();
     currentCard = cardCountOnSlide;
+    countSlide.textContent = currentCard;
     totalGap = getTotalGap();
     cardSize = calcCardSize();
     translateShift = 0;
     document.documentElement.style.setProperty('--cardSize', cardSize + 'px');
     sliderList.style.translate = -(cardSize + gapSize) * translateShift + 'px';
-    console.log(translateShift);
 }
 
 function getCardCountOnSlide() {
@@ -54,24 +61,31 @@ function calcHiddenCardCount() {
 }
 
 function moveLeft() {
-    if (currentCard + 1 > TOTAL_CARD_COUNT) {
+    if (currentCard + 1 > TOTAL_CARD_COUNT) rightBtn.disabled = false;
+
+    if (currentCard - 1 < cardCountOnSlide) {
+        leftBtn.disabled = true;
         return true;
     }
-    if (currentCard + 1 === TOTAL_CARD_COUNT) {
-        left.disabled = true;
-    }
-    currentCard++;
+    // if (currentCard - 1 === cardCountOnSlide) {
+    //     leftBtn.disabled = true;
+    // }
+    currentCard--;
+    countSlide.textContent = currentCard;
     sliderList.style.translate = -((cardSize + gapSize) * currentCard - (cardSize + gapSize) * cardCountOnSlide) + 'px';
 }
 
 function moveRight() {
-    if (currentCard - 1 < cardCountOnSlide) {
-        return;
+    if (currentCard + 1 >= cardCountOnSlide) leftBtn.disabled = false;
+    if (currentCard + 1 > TOTAL_CARD_COUNT) {
+        rightBtn.disabled = false;
+        return true;
     }
-    if (currentCard - 1 === cardCountOnSlide) {
-        right.disabled = true;
+    if (currentCard + 1 === TOTAL_CARD_COUNT) {
+        rightBtn.disabled = true;
     }
-    currentCard--;
+    currentCard++;
+    countSlide.textContent = currentCard;
     sliderList.style.translate = -((cardSize + gapSize) * currentCard - (cardSize + gapSize) * cardCountOnSlide) + 'px';
 }
 
@@ -84,7 +98,8 @@ playersBtns.addEventListener('click', (e) => {
 });
 
 setInterval(() => {
-    if (moveLeft()) {
+    if (moveRight()) {
         currentCard = cardCountOnSlide;
+        countSlide.textContent = currentCard;
     }
-}, 2000);
+}, 4000);
